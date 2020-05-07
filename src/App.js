1160592b68e0ac898e0cbface956e703db16fca5
JashanB <jashan_brar50@hotmail.com>
2020-05-07 19:37:09 -0400

    added skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React, { useState } from 'react';
 import './App.css';
 
+//email needs to be vaild
+//can only be submitted once
+//once submitted - delay 2 seconds then show Thank you 
+//second form is drop down menu 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header">INTERNSHIP SIGNUP FORM</h1>
+      <p>Prepare for your career with a Project Management, Web-Development, Graphic Design, or Digital Marketing Internship at Northern</p>
+      <div className="forms">
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name={search}
+            placeholder="Search here"
+            onChange={(event) => {
+              setSearch(event.target.value)
+              setError('');
+            }
+            }
+            value={search}
+          />
+          <section className="search-validation">{error}</section>
+        </form>
+        <div class="dropdown">
+          <button onclick={dropDown} className="dropbtn">Your Interests</button>
+          <div className="dropdown-content">
+            <button className="select-options" onClick={interest}>Project Management</button>
+            <button className="select-options" onClick={interest}>Web-Development</button>
+            <button className="select-options" onClick={interest}>Graphic Design</button>
+            <button className="select-options" onClick={interest}>Digital Marketing</button>
+          </div>
+        </div>
+      </div>
+            <button>Sign Up Now</button>
     </div>
   );
 }
